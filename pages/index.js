94e0719d69519ab04218894d636bbd9d848d234e
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-07-24 11:54:58 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function HomeScreen() {
   return (
     <div style={{ backgroundColor: "gray" }}>
-      <h1>Página inicial</h1>
+      <h1>Página inicial [NOVA ALTERAÇÃO TESTE]</h1>
       <h3>TESTANDO CI E CD</h3>
     </div>
   );
--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -3,6 +3,9 @@ const { execSync } = require("child_process")
 console.log("[DEPLOY_PREVIEW]: START");
 const command = 'yarn deploy:staging';
 const output = execSync(command, { encoding: "utf8" });
-console.log(output);
+const outputLines = output.split("\n")
+const DEPLOY_URL = [outputLines.length - 1]
 console.log("[DEPLOY_PREVIEW]: END");
+
+console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
 
