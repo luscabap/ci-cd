a1762f93e9c7266e7775fc895c0818d01a34577d
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-07-24 10:12:05 -0300

    update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,3 +1,8 @@
 /* eslint-disable prettier/prettier */
+import { execSync } from "child_process"
+console.log("[DEPLOY_PREVIEW]: START");
+const command = 'yarn deploy:staging';
+const output = execSync(command, { encoding: "utf8" });
+console.log(output);
+console.log("[DEPLOY_PREVIEW]: END");
 
-console.log("Hello world!");
