48ed862eb18f73984760c15efc2f109a51b6cc1b
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-07-25 10:59:06 -0300

    forcing error

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export default function HomeScreen() {
     <div style={{ backgroundColor: "gray" }}>
       <h1>Página inicial [NOVA ALTERAÇÃO TESTE] mauz</h1>
       <h2>Alteração teste de preview e deploy</h2>
-      <h3>TESTANDO CI E CD</h3>
+  <h3>TESTANDO CI E CD</h3>
     </div>
   );
 }
